7daab414ce16251af1bc58d528763d9fd0c7b4cc
jorgealexis07 <201711104@tese.edu.mx>
2022-05-17 19:50:37 -0500

    Agrego operaciones CRUD

--- server.js
+++ server.js
@@ -55,6 +55,53 @@ app.delete('/explorers/:id', async (req, res) => {
 	return res.json({message: "Eliminado correctamente"});
 });
 
+//
+
+app.get('/alumnos', async (req, res) => {
+    const allAlumnos =  await prisma.alumno.findMany({});
+    res.json(allAlumnos);
+});
+
+app.get('/alumnos/:id', async (req, res) => {
+  const id = req.params.id;
+  const alumno = await prisma.alumno.findUnique({where: {id: parseInt(id)}});
+  res.json(alumno);
+});
+
+app.post('/alumno', async (req, res) => {
+    const alumno = {
+      name: req.body.name,
+      lang: req.body.lang,
+      missionCommander: req.body.missionCommander,
+      enrrollments: req.body.enrrollments
+     };
+    const message = 'Alumno creado.';
+    await prisma.explorer.create({data: alumno});
+    return res.json({message});
+  });
+
+  app.put('/alumnos/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+
+	await prisma.alumno.update({
+		where: {
+			id: id
+		},
+		data: {
+            name: req.body.name,
+            missionCommander: req.body.missionCommander,
+		}
+	})
+	return res.json({message: "Actualizado correctamente"});
+});
+
+app.delete('/alumnos/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+	await prisma.explorer.delete({where: {id: id}});
+	return res.json({message: "Eliminado correctamente"});
+});
+
+
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
