80a489e9b3454582490cbec55602c472f80903bd
jorgealexis07 <201711104@tese.edu.mx>
2022-05-26 19:38:11 -0500

    Agrego la implementacion de codigo de cors en el server

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
-
 // Require para usar Prisma
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+//Cors
+const cors = require("cors");
+
+const corsOptions = { origin: "http://localhost:8081" };
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.json({message: 'alive'});
@@ -100,6 +106,8 @@ app.delete('/alumnos/:id', async (req, res) => {
 	await prisma.explorer.delete({where: {id: id}});
 	return res.json({message: "Eliminado correctamente"});
 });
+
+
 
 
 
